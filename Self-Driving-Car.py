464f0460cf436c8570452e46598ece847a4d9663
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 14:45:07 +0530

    track-1 initialize

---
+++ Self-Driving-Car.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode((1200, 400))
+track = pygame.image.load('track1.png')
+drive = True
+
+while drive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drive = False
+    window.blit(track, (0, 0))
+    pygame.display.update()
