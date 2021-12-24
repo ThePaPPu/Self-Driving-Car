6fd3d164e1218179d08bb2de11b05e4f6f06d388
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 16:32:44 +0530

    Car on the track

--- Self-Driving-Car.py
+++ Self-Driving-Car.py
@@ -4,6 +4,11 @@ pygame.init()
 
 window = pygame.display.set_mode((1200, 400))
 track = pygame.image.load('track1.png')
+car = pygame.image.load('tesla.png')
+car = pygame.transform.scale(car, (30, 60))
+car_x = 152
+car_y = 280
+
 drive = True
 
 while drive:
@@ -11,4 +16,5 @@ while drive:
         if event.type == pygame.QUIT:
             drive = False
     window.blit(track, (0, 0))
+    window.blit(car, (car_x, car_y))
     pygame.display.update()
