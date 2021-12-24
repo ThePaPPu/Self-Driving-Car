dfa5e40ae289f92a88151a289599e2cedf223be1
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 16:35:27 +0530

    setup camera in the car

--- Self-Driving-Car.py
+++ Self-Driving-Car.py
@@ -16,8 +16,11 @@ while drive:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drive = False
-    
+            
+    camera_x = car_x + 15
+    camera_y = car_y + 15
     car_y = car_y - 2
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
+    pygame.draw.circle(window, (0, 255, 0), (camera_x, camera_y), 5, 5)
     pygame.display.update()
