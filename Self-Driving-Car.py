e1bad626f3af89ed40bc0f3930ee46c218e8f7d3
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 16:34:09 +0530

    car running on the track

--- Self-Driving-Car.py
+++ Self-Driving-Car.py
@@ -8,13 +8,16 @@ car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 152
 car_y = 280
-
 drive = True
+clock = pygame.time.Clock()
 
 while drive:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drive = False
+    
+    car_y = car_y - 2
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
     pygame.display.update()
