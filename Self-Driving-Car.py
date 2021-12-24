5001c4c7cf59ed51c74c57768160e90df3312d2c
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 16:43:41 +0530

    car running and stopped detect by road
    Here, Road = White = 255.
    So when car detect 255 by this camera then the car will only run.

--- Self-Driving-Car.py
+++ Self-Driving-Car.py
@@ -8,6 +8,7 @@ car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 152
 car_y = 280
+focal_distance = 25
 drive = True
 clock = pygame.time.Clock()
 
@@ -16,10 +17,16 @@ while drive:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             drive = False
-            
+    
     camera_x = car_x + 15
     camera_y = car_y + 15
-    car_y = car_y - 2
+    
+    #detect road
+    upper_pixel = window.get_at((camera_x, camera_y - focal_distance))[0]
+    print(upper_pixel)
+    
+    if upper_pixel == 255:
+        car_y = car_y - 2
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
     pygame.draw.circle(window, (0, 255, 0), (camera_x, camera_y), 5, 5)
