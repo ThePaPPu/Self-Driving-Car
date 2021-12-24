936dfbe417941162d8cf28050028bcfad4fd9859
The Pappu <58678139+ThePaPPu@users.noreply.github.com>
2021-12-24 18:18:24 +0530

    Track-4 completed
    Detect road upper to down side and continue car run

--- Self-Driving-Car.py
+++ Self-Driving-Car.py
@@ -3,12 +3,13 @@ import pygame
 pygame.init()
 
 window = pygame.display.set_mode((1200, 400))
-track = pygame.image.load('track3.png')
+track = pygame.image.load('track4.png')
 car = pygame.image.load('tesla.png')
 car = pygame.transform.scale(car, (30, 60))
 car_x = 152
 car_y = 280
 camera_x_offset = 0
+camera_y_offset = 0
 focal_distance = 25
 direction = 'upper'
 drive = True
@@ -21,26 +22,39 @@ while drive:
             drive = False
     
     camera_x = car_x + camera_x_offset + 15
-    camera_y = car_y + 15
+    camera_y = car_y + camera_y_offset + 15
     
     #detect road
     upper_pixel = window.get_at((camera_x, camera_y - focal_distance))[0]
     right_pixel = window.get_at((camera_x + focal_distance, camera_y))[0]
-    print(upper_pixel, right_pixel)
+    down_pixel = window.get_at((camera_x , camera_y + focal_distance))[0]
+    print(upper_pixel, right_pixel, down_pixel)
     
     #take turn or change direction
     if direction == 'upper' and upper_pixel != 255 and right_pixel == 255:
         direction = 'right'
         camera_x_offset = 30
         car = pygame.transform.rotate(car, -90)
+    
+    elif direction == 'right' and right_pixel != 255 and down_pixel == 255:
+        direction = 'down'
+        car_x = car_x + 30
+        camera_x_offset = 0
+        camera_y_offset = 30
+        car = pygame.transform.rotate(car, -90)
+        
+        
         
         
     #drive
     if direction == 'upper' and upper_pixel == 255:
         car_y = car_y - 2
         
-    if direction == 'right' and right_pixel == 255:
+    elif direction == 'right' and right_pixel == 255:
         car_x = car_x + 2
+    
+    elif direction == 'down' and down_pixel == 255:
+        car_y = car_y + 2 
         
     window.blit(track, (0, 0))
     window.blit(car, (car_x, car_y))
